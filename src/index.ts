b5e37dfa8084273ec557c56119f6818313c7c550
takashiuesaka <61622933+takashiuesaka@users.noreply.github.com>
2022-12-24 12:03:34 +0000

    Default Setup

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import readline from "readline";
+
+export const Index = (): void => {
+    const [, , firstArg] = process.argv;
+
+    if (!firstArg) {
+        console.error("Please pass one argument!!");
+        process.exit(1);
+    }
+
+    const msg = `
+    Hello!! ${firstArg} san.
+    I am Toshihisa Tomatsu.
+    GitHub: https://github.com/toshi-toma
+    Twitter: https://twitter.com/toshi__toma
+  `;
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    console.log(msg);
+
+    rl.question("Please enter names for your project: ", answer => {
+        console.log(`Thank you!! Let's start ${answer}`);
+
+        rl.close();
+    });
+};
+
+Index();
+
